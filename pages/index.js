b444fdb138c8db38c8de884706d62d5868135cf3
Leo Shih <leoshih@qnap.com>
2023-05-21 15:18:49 +0800

    Static generation with fetching external files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from "../lib/posts";
+import Layout, { siteTitle } from "../components/layout";
 import styles from "../styles/Home.module.css";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ allPosts }) {
 	return (
 		<Layout home>
 			<Head>
@@ -11,12 +12,39 @@ export default function Home() {
 			</Head>
 
 			<section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+				<p>[Your Self Introduction]</p>
+				<p>
+					(This is a sample website - you’ll be building a site like this on{" "}
+					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+				</p>
+			</section>
+
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{
+						allPosts.map(({ id, date, title }) => 
+							<li className={utilStyles.listItem} key={id}>
+								{title}
+								<br />
+								{id}
+								<br />
+								{date}
+							</li>
+						)
+					}
+				</ul>
+			</section>
 		</Layout>
 	);
+}
+
+export function getStaticProps() {
+	const allPosts = getSortedPostsData();
+
+	return {
+		props: {
+			allPosts,
+		},
+	};
 }
